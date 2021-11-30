2d58ed083bf542e38e73354a7e68bdf7881bcdad
Oleksandr Bykbaiev <oleksandr.b@attendify.com>
2021-11-30 22:25:56 +0200

    feat(layout): add blank Header & Menu components
    Only templates (WIP)
    
    feat/layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,18 @@
-import { H1 } from '../Title';
+import { Flex } from 'components/Flex';
+import { Logo } from 'components/Logo';
+import { H1 } from 'components/Title';
+import { Menu } from 'containers/Menu';
 
 export const Header = () => (
+  <Flex justifyContent="space-between" alignItems="center">
     <H1>Header</H1>
+    <Logo />
+    <Menu />
+  </Flex>
 );
+
+/* Logo */
+/* Menu */
+/* Search */
+/* Watchlist */
+/* Login */
---
+++ src/containers/Menu/Menu.tsx
@@ -0,0 +1,3 @@
+import { Text } from 'components/Text';
+
+export const Menu = () => (<Text>Menu</Text>);
