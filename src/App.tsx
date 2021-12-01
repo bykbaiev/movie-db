7318817b7e7bad5b65be3842750ca9a649c2c1ca
Oleksandr Bykbaiev <oleksandr.b@attendify.com>
2021-12-01 02:22:26 +0200

    feat(upcoming-movies): add suspense
    For future handling of loader (if it wouldn't be handled locally in pages/containers)
    
    feat/upcoming-movies

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { Routes } from 'Routes';
+import { Suspense } from 'react';
 
 export function App() {
   return (
     <Router>
       <RecoilRoot>
         <ChakraProvider resetCSS>
+        <Suspense fallback={<div>Loading...</div>}>
           <div className="App">
             <Routes />
           </div>
+          </Suspense>
         </ChakraProvider>
       </RecoilRoot>
     </Router>
