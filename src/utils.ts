a0f698872ef1659b34c80f859c41b894ce202822
Oleksandr Bykbaiev <oleksandr.b@attendify.com>
2021-12-15 03:32:42 +0200

    feat(upcoming-movies): add mechanism to get full poster path
    feat/upcoming-movies

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { IMAGE_BASE_URL } from 'api';
 import { ChangeEvent } from 'react';
 
 type UnknownFn<R = any> = (...args: any) => R;
@@ -6,3 +7,7 @@ export const compose = <R>(...fns: Array<UnknownFn>): UnknownFn<R> =>
   fns.reduce((composed, fn) => (...args) => composed(fn(...args)));
 
 export const getTargetValue = (event: ChangeEvent<HTMLInputElement>) => event.currentTarget.value;
+
+export const getImagePath = (filePath: string | null | undefined): string | null => (filePath
+? `${IMAGE_BASE_URL}${filePath}`
+: null);
